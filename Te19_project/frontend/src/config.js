41c3f2fcdab127c4ea9f405a63a7752f6335ca23
Livie-Ng <mngg0050@student.monash.edu>
2025-08-12 00:10:51 +1000

    Update config.js

--- Te19_project/frontend/src/config.js
+++ Te19_project/frontend/src/config.js
@@ -1,9 +1,5 @@
 const config = {
-  apiUrl: process.env.NODE_ENV === 'production' 
-    ? process.env.VUE_APP_API_URL
-    : 'http://localhost:8000',  // Your Django backend
-  
-  environment: process.env.VUE_APP_ENVIRONMENT || 'development'
+  apiUrl: 'http://127.0.0.1:8000',  // Django backend
+  environment: 'development'
 }
-
 export default config
