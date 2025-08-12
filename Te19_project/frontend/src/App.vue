bd532b9bad200be20139fbc3b83f04a275e98524
Livie-Ng <mngg0050@student.monash.edu>
2025-08-12 11:38:35 +1000

    app.vue

--- Te19_project/frontend/src/App.vue
+++ Te19_project/frontend/src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <!-- Your existing template content -->
     <router-view />
   </div>
 </template>
 
 <script>
-import { parkingAPI } from '@/services/api'
-import config from '@/config'
-
 export default {
   name: 'App',
-  async mounted() {
-    console.log('API URL:', config.apiUrl)  // Should show http://localhost:8000
-    
-    try {
-      const response = await parkingAPI.getParkingData()
-      console.log('Data:', response.data)
-    } catch (error) {
-      console.error('API Error:', error)
-    }
+  mounted() {
+    // Just log the environment for debugging
+    console.log('=== APP MOUNTED ===')
+    console.log('Environment:', process.env.NODE_ENV)
+    console.log('API URL:', process.env.VUE_APP_API_URL)
   }
 }
 </script>
 
 <style>
-/* Your existing styles */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
